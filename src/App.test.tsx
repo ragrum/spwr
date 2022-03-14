a10e41ea417ae29f07ebb9cba438b1835754fae9
José Ríos <ragrum@protonmail.com>
2022-03-14 16:29:36 -0600

    Add test for modal

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn more about SPWR button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn more about spwr/i);
-  expect(linkElement).toBeInTheDocument();
+  const modalButton = screen.getByText(/learn more about spwr/i);
+  expect(modalButton).toBeInTheDocument();
+});
+
+test('displays modal with info about SPWR', () => {
+  render(<App />);
+  const modalButton = screen.getByText(/learn more about spwr/i);
+  fireEvent.click(modalButton);
+  const modalContent = screen.getByText(
+    /spwr corporation is a public company/i
+  );
+  expect(modalContent).toBeInTheDocument();
 });
