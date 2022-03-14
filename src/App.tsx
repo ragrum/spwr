151ac85cff6f9dfcaa875be63851ff87f62cca77
José Ríos <ragrum@protonmail.com>
2022-03-14 12:26:02 -0600

    Control Modal with React's state

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Modal } from './components';
 
 import sun from './sun.svg';
 
 function App() {
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const open = () => setShowModal(true);
+  const close = () => setShowModal(false);
+
   return (
     <div className="text-center">
-      <header className="App-header bg-gray-800 text-white min-h-screen flex flex-col items-center justify-center">
+      <header className="App-header bg-gray-700 text-white min-h-screen flex flex-col items-center justify-center">
         <img
           src={sun}
           className="h-[25vmin] pointer-events-none motion-safe:animate-[spin_20s_linear_infinite]"
           alt="sun"
         />
 
-        <p>
-          Edit <code className="font-mono">src/App.tsx</code> and save to
-          reload.
-        </p>
-
-        <a
-          className="text-cyan-300"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          className="mt-12 p-2.5 rounded border border-amber-500 cursor-default bg-amber-600 text-white shadow-md shadow-amber-500/30"
+          onClick={open}
         >
           Learn more about SPWR
-        </a>
+        </button>
 
-        <Modal />
+        <Modal isOpen={showModal} onDismiss={close} />
       </header>
     </div>
   );
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,9 +3,18 @@ import { Dialog } from '@reach/dialog';
 
 import '@reach/dialog/styles.css';
 
-function Modal() {
+interface Props {
+  isOpen: boolean;
+  onDismiss: () => void;
+}
+
+function Modal({ isOpen, onDismiss }: Props) {
   return (
-    <Dialog isOpen={true} aria-label="Learn more about SPWR">
+    <Dialog
+      isOpen={isOpen}
+      onDismiss={onDismiss}
+      aria-label="Learn more about SPWR"
+    >
       <p>Some Content</p>
     </Dialog>
   );
